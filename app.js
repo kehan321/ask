28bd5543064d1ff6248f2d5e32454e8ef2cdb7b1
kehan <skyrrah999@gmail.com>
2022-10-29 20:15:13 +0500

    js01

---
+++ app.js
@@ -0,0 +1,106 @@
+// chapter no 01     "ALERT"
+
+alert("Popups are used in some exercises. Please disable your popup blocker for this site.");
+window.alert("A Smarter Way to Learn JavaScript.pdf");
+
+
+// chapter no 02   "Variables for Strings"
+
+
+
+var thanx = "Thanks for your input!"
+alert(thanx);
+
+var name = "kehan"
+console.log(name)
+
+
+// chapter no 03   "Variables for Strings"
+
+var originalNum = 23;
+var numToBeAdded = 7;
+var newNum = originalNum + numToBeAdded;
+console.log(newNum);
+
+originalNum = 90;
+originalNum = originalNum + 10;
+
+console.log(originalNum);
+
+// chapter no 04   "Variable Names Legal and Illegal"
+
+var -a = 2;   //illegal
+var _a = 2;   //legal
+var *a = 2;   //illegal
+var anand = 2;   //legal
+var -1anand = 2;   //illegal
+var a34 = 2;   //legal
+
+
+
+
+// chapter no 05   "Math expressions:Familiar operators"
+
+var num = 10;
+var popularNumber = num + 200;
+
+alert(popularNumber);
+
+
+
+var num = 10;
+var anotherNum = 1;
+var popularNumber = num + anotherNum;
+
+
+alert(popularNumber);
+
+
+// chapter no 06   "Math expressions:UnFamiliar operators"
+
+var num = 1;
+var newNum = --num;
+
+alert(newNum);
+var num = 1;
+var newNum = ++num;
+
+alert(newNum);
+var num = 1;
+var newNum = num--;
+
+alert(newNum);
+var num = 1;
+var newNum = num++;
+
+alert(newNum);
+
+// chapter no 07   "Math expressions:Eliminating ambiguity"
+
+
+var a = 12;
+var b = 10;
+
+
+var c = a++ - --b + 30 -43 -b +a*b;
+
+alert(c);
+
+// chapter no 08   "Concatenating text strings"
+
+var a = 12;
+ var b = "20"
+
+var c = a-b;
+
+console.log(c)
+
+var c = a+b;
+
+console.log(c)
+
+var a = "Pakis"
+var  b = "tan"
+var c = a+b;
+
+console.log(c)
